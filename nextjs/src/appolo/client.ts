626457cd3764326fc2cd69825143fb9fa7f95642
inoue2002 <inoue2002@icloud.com>
2024-07-26 21:57:53 +0900

    feat codegenしたコードでDBにデータを登録できるように

---
+++ nextjs/src/appolo/client.ts
@@ -0,0 +1,11 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_HASURA_ENDPOINT,
+  cache: new InMemoryCache(),
+  headers: {
+    'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET || '',
+  },
+});
+
+export default client;
